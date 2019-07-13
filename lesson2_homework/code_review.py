530fea1f43f6daeef98d312af2f7a890fd831bc6
zhanghengruiC <zhanghengruic@163.com>
2019-07-13 10:40:06 +0800

    done

--- lesson2_homework/code_review.py
+++ lesson2_homework/code_review.py
@@ -1,6 +1,6 @@
 from __future__ import unicode_literals
 import json
-
+from pprint import pprint
 from lxml import etree
 import requests
 base_url = 'http://bj.bendibao.com/ditie/linemap.shtml'
@@ -16,22 +16,20 @@ for subway_obj in subway_objs:
     subway_stations = subway_obj.xpath('.//div[@class="station"]/a/text()')
     subway_items.update({subway_line:subway_stations})
 print(subway_items)
-last_station = None
-for subway_line,subway_stations in subway_items.items():
-    stations = []
-    for station in subway_stations:
-        # if station not in station_dict.keys() and last_station != None:
-        if station not in station_dict.keys():
-            station_dict.setdefault(station,[])
-            if last_station:
-                station_dict.get(last_station).append(station)
-                station_dict.get(station).append(last_station)
-        elif station not in station_dict.get(last_station):
-            station_dict.get(last_station).append(station)
-        elif last_station not in station_dict.get(station):
-            station_dict.get(station).append(last_station)
-        last_station = station
 
-with open('2.json','w') as fp:
-    json_obj = json.dumps(station_dict,ensure_ascii=False)
-    fp.writelines(json_obj)
+for subway_line,subway_stations in subway_items.items():
+    last_station = None
+    for station in subway_stations:
+        if station not in station_dict.keys():
+            station_dict.setdefault(station,set())
+            if last_station:
+                station_dict.get(last_station).add(station)
+                station_dict.get(station).add(last_station)
+        elif last_station:
+            station_dict.get(last_station).add(station)
+            station_dict.get(station).add(last_station)
+        else:
+            pass
+        last_station = station
+# print(station_dict)
+pprint(station_dict)
